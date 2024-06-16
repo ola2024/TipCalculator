fb006c439823228aac469190db0270c06ffd3eca
“ola <waleabi764@gmail.com>
2024-06-16 01:38:03 -0400

    Creating container composable function "Myapp"

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -16,24 +16,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MoviesappTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-
-                }
-            }
+           Myapp {
+               MainContent()
+           }
         }
     }
 }
+@Composable
+fun Myapp(content:@Composable ()-> Unit){
+    MoviesappTheme {
+        content()
+    }
+}
 
-
+@Composable
+fun MainContent(){
+    Text(text = "Hello World")
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MoviesappTheme {
-
+       Myapp {
+           MainContent()
+       }
     }
 }
