eda7bdf6bd48b1de25045ab43a4a22e2796b3d76
“ola <waleabi764@gmail.com>
2024-06-17 23:42:38 -0400

    Adding details page and display data

--- app/src/main/java/com/example/moviesapp/components/widget/DetailsScreen.kt
+++ app/src/main/java/com/example/moviesapp/components/widget/DetailsScreen.kt
@@ -1,36 +1,60 @@
 package com.example.moviesapp.components.widget
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.moviesapp.navigation.screenroute.ScreenRoute
+import com.example.moviesapp.model.getDummyMovie
 
 @Composable
-fun DetailsScreen(navController: NavController, moviesData: String?) {
-    println("What is inside this: $moviesData")
-    Surface(
+fun DetailsScreen(
+    navController: NavController,
+    moviesId: String?,
+) {
+    val moviesList = getDummyMovie().filter { it.movieImdbID == moviesId }
+    Card(
         modifier = Modifier
-            .clickable {
-                navController.navigate(route = ScreenRoute.HomeScreen.name)
-            }
-            .fillMaxHeight()
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .height(120.dp),
         shape = RectangleShape,
-        color = Color.LightGray
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 6.dp),
+        colors = CardDefaults.cardColors(Color.Transparent)
     ) {
-        moviesData?.let {
-            Text(text = it, modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier.padding(start = 20.dp, top = 40.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            moviesList.firstOrNull()?.let {
+                Text(text = buildAnnotatedString {
+                    withStyle(
+                        SpanStyle(
+                            color = Color.Black,
+                            fontSize = 16.sp,
+                            fontStyle = FontStyle.Normal,
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
+                        append(it.movieGenre)
+                    }
+                })
+            }
         }
 
     }
-
 }
--- app/src/main/java/com/example/moviesapp/components/widget/MainContentRow.kt
+++ app/src/main/java/com/example/moviesapp/components/widget/MainContentRow.kt
@@ -1,5 +1,7 @@
 package com.example.moviesapp.components.widget
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,43 +11,53 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
+import com.example.moviesapp.model.MovieItem
+import com.example.moviesapp.model.getDummyMovie
 import com.example.moviesapp.navigation.screenroute.ScreenRoute
 
 @Composable
 fun MainContentRow(
     modifier: Modifier = Modifier,
-    title: String,
-    imageVector: ImageVector,
+    title: MovieItem,
     onItemClick: () -> Unit
 ) {
+    var expanded = remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = modifier
             .fillMaxWidth()
             .clickable { onItemClick.invoke() }
             .padding(2.dp)
             .size(100.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent),
         shape = RoundedCornerShape(corner = CornerSize(6.dp)),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
@@ -57,49 +69,125 @@ fun MainContentRow(
             Surface(
                 modifier = Modifier
                     .padding(12.dp)
-                    .size(100.dp),
-                color = Color.LightGray
+                    .size(80.dp),
+                shape = CircleShape
             ) {
-                Icon(imageVector = imageVector, contentDescription = "Image")
+                Image(
+                    painter = rememberImagePainter(data = title.movieImages[0], builder = {
+                        crossfade(true)
+                        transformations(CircleCropTransformation())
+                    }),
+                    contentDescription = "images"
+                )
             }
-            Text(
-                text = title,
-                modifier = Modifier,
-                style = TextStyle(color = Color.Black),
-                fontStyle = FontStyle(10),
-                fontWeight = FontWeight.ExtraBold
-            )
+            Column(modifier = Modifier) {
+                Text(text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.Black,
+                            fontFamily = FontFamily.SansSerif,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.ExtraBold
+                        )
+                    ) {
+                        append("Title : ")
+                    }
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.DarkGray,
+                            fontStyle = FontStyle.Normal,
+                            fontFamily = FontFamily.Monospace,
+                            fontSize = 13.sp
+                        )
+                    ) {
+                        append(title.movieTitle)
+                    }
+                }, modifier = Modifier.clickable { expanded.value = !expanded.value })
+                AnimatedVisibility(visible = expanded.value) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Black,
+                                    fontFamily = FontFamily.SansSerif,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.ExtraBold
+                                )
+                            ) {
+                                append("Rating: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontStyle = FontStyle.Normal,
+                                    fontFamily = FontFamily.Monospace,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append(title.movieRate)
+                            }
+                        })
+                        Text(text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Black,
+                                    fontFamily = FontFamily.SansSerif,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.ExtraBold
+                                )
+                            ) {
+                                append("Year: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontStyle = FontStyle.Normal,
+                                    fontFamily = FontFamily.Monospace,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append(title.movieYear)
+                            }
+                        })
+                        Text(text = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Black,
+                                    fontFamily = FontFamily.SansSerif,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.ExtraBold
+                                )
+                            ) {
+                                append("Released: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontStyle = FontStyle.Normal,
+                                    fontFamily = FontFamily.Monospace,
+                                    fontSize = 13.sp
+                                )
+                            ) {
+                                append(title.movieReleased)
+                            }
+                        })
+                    }
+                }
+            }
         }
-    }
 
+    }
 }
+
 
 @Composable
 fun HomeScreen(
-    itemsList: List<String> = listOf(
-        "Room To Go",
-        "Blad Runner",
-        "Vikings Room",
-        "Day of the Jacker",
-        "Woman Go To School",
-        "Room To Go",
-        "Blad Runner",
-        "Vikings Room",
-        "Day of the Jacker",
-        "Woman Go To School",
-        "Room To Go",
-        "Blad Runner",
-        "Vikings Room",
-        "Day of the Jacker",
-        "Woman Go To School",
-        "Room To Go",
-        "Blad Runner",
-        "Vikings Room",
-        "Day of the Jacker",
-        "Woman Go To School"
-    ),
+    itemsList: List<MovieItem> = getDummyMovie(),
     navController: NavController,
-    onItemClick:(String) -> Unit
+    onItemClick: (String) -> Unit
 ) {
     Column(modifier = Modifier.padding(10.dp)) {
         LazyColumn(
@@ -109,24 +197,23 @@ fun HomeScreen(
                 MainContentRow(
                     title = it,
                     onItemClick = {
-                        navController.navigate(route = ScreenRoute.DetailsScreen.name + "/$it")
+                        navController.navigate(route = ScreenRoute.DetailsScreen.name + "/${it.movieImdbID}")
                     },
-                    imageVector = Icons.Default.AccountBox
                 )
             }
         }
-
     }
 
 }
+
 @ExperimentalMaterial3Api
 @Composable
 fun TopAppBarComposable(
-    text:String
+    text: String
 ) {
     TopAppBar(
         title = { Text(text = text) },
-        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.LightGray),
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Transparent),
         scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
     )
 }
